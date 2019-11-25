e5703a7770f45399cd830bdb624e5b7cfe2823d8
ayuafida <ayuafidarisarahma18@gmail.com>
2019-11-25 16:50:49 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+
+import Nama from './prop/Prop';
+import Box from '@material-ui/core/Box';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+
+ class App extends Component {
+   constructor(props){
+     super(props);
+     this.state ={
+       nama : '' ,
+     }
+   }
+   handleSubmit =(event) => {
+     let nama = event.target.name;
+     let val = event.target.value;
+     this.setState({
+       [nama] : val,
+     });
+   }
+
+   handleNama =(event) => {
+    let nama = event.target.name;
+    let val = event.target.value;
+    this.setState({
+      [nama] : val,
+    });
+  }
+   render(){
+     
+    return(
+       
+    <div className="App">
+    <form onSubmit={this.handleSubmit}>
+      <Box component="span" display="block">
+        <TextField
+        name="nama"
+        id="standart-with-placeholder"
+        label="Nama Lengkap"
+        placeholder="Masukkan Nama"
+        margin="normal"
+        value = {this.state.nama}
+        onChange={this.handleNama}
+        />
+      </Box>
+      <Box component="span" display="block">
+        <Button variant="contained" color="primary" type="submit">
+          Ganti Nama
+        </Button>
+      </Box>
+
+    </form>
+<Nama nama ={this.state.nama} />
+  
+  </div>
+     );
+   }
+
+   }
+  
+
+
+export default App;
---
+++ src/prop/Prop.jsx
@@ -0,0 +1,11 @@
+import React ,{Component}from 'react';
+
+const Nama = (props) => {
+    return(
+    <div class="App"  >
+        <h1> Hello {props.nama}</h1>
+    </div>
+     
+    )
+}
+export default Nama;
